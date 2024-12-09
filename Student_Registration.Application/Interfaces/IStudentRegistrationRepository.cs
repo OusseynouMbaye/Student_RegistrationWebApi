046402ffb667d149304040c26ba81e865fcfedf6
Ouzinem <ouzinem@hotmail.com>
2024-12-09 11:14:11 -0500

    Add IStudentRegistrationRepository and its implementation

---
+++ Student_Registration.Application/Interfaces/IStudentRegistrationRepository.cs
@@ -0,0 +1,12 @@
+﻿using Student_Registration.Domain;
+
+namespace Student_Registration.Application.Interfaces
+{
+    public interface IStudentRegistrationRepository
+    {
+        Task<IEnumerable<Student>> GetStudentsAsync();
+        Task<Student> GetStudentByIdAsync(int id);
+        //Task<int> CreateStudentAsync(Student student);
+        Task<int> CreateStudentAsync(Student student);
+    }
+}
---
+++ Student_Registration.Infrastructure/Repositories/StudentRegistrationRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Registration.Application.Interfaces;
+using Student_Registration.Domain;
+using Student_Registration.Infrastructure.Context;
+
+namespace Student_Registration.Infrastructure.Repositories
+{
+    public class StudentRegistrationRepository : IStudentRegistrationRepository
+    {
+        private readonly StudentRegistrationDbContext _studentRegistrationContext;
+
+        public StudentRegistrationRepository(StudentRegistrationDbContext studentRegistrationContext)
+        {
+            _studentRegistrationContext = studentRegistrationContext ?? throw new ArgumentNullException(nameof(studentRegistrationContext));
+        }
+
+        public async Task<IEnumerable<Student>> GetStudentsAsync()
+        {
+            return await _studentRegistrationContext.Students.ToListAsync();
+        }
+
+        public Task<Student> GetStudentByIdAsync(int id)
+        {
+            return _studentRegistrationContext.Students.FirstOrDefaultAsync(s => s.id == id);
+        }
+
+        //public async Task CreateStudentAsync(Student student)
+        //{
+        //    _studentRegistrationContext.Add(student);
+        //    await _studentRegistrationContext.SaveChangesAsync();
+        //}
+
+       public async Task<int> CreateStudentAsync(Student student)
+        {
+           _studentRegistrationContext.Add(student);
+           return await _studentRegistrationContext.SaveChangesAsync();
+        }
+    }
+}
