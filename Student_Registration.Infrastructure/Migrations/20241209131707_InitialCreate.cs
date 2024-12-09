63c38877c0370055b16dcec95cfbb55e56aefb89
Ouzinem <ouzinem@hotmail.com>
2024-12-09 08:21:45 -0500

    Add initial migration for student registration

---
+++ Student_Registration.Infrastructure/Migrations/20241209131707_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Student_Registration.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: true),
+                    age = table.Column<int>(type: "INTEGER", nullable: false),
+                    address = table.Column<string>(type: "TEXT", nullable: true),
+                    gender = table.Column<string>(type: "TEXT", nullable: true),
+                    phoneNum = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
