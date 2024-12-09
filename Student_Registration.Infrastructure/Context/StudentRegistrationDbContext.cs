b60c4b14053734d80bb623f906c07f89d8f27a27
Ouzinem <ouzinem@hotmail.com>
2024-12-09 08:16:53 -0500

    Add project files.

---
+++ Student_Registration.Infrastructure/Context/StudentRegistrationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Registration.Domain;
+
+namespace Student_Registration.Infrastructure.Context
+{
+    public class StudentRegistrationDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public StudentRegistrationDbContext(DbContextOptions<StudentRegistrationDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ Student_Registration.Webui/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Student_Registration.Infrastructure.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File("logs/StudentRegistration.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContextFactory<StudentRegistrationDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("StudentResgistrationConnection"));
+});
+
+builder.Host.UseSerilog();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
