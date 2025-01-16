5bd3d0e51974acfee7b26e7f6c0d992d12ff4137
Ouzinem <ouzinem@hotmail.com>
2025-01-16 07:38:14 -0500

    Refactor StudentForCreation and add FluentValidation
    Refactored StudentForCreation from a positional record to a standard class with properties, including commented-out data annotations for validation. Updated Program.cs to include FluentValidation library, added necessary using directives, and registered FluentValidation services in the dependency injection container. Made a minor formatting change to the AddControllers method call in Program.cs. Updated Student_Registration.Webui.csproj to include FluentValidation.AspNetCore package version 11.3.0. Introduced StudentForCreationValidator class to validate StudentForCreation DTO using FluentValidation.

--- Student_Registration.Webui/Dtos/StudentsDtos/StudentForCreation.cs
+++ Student_Registration.Webui/Dtos/StudentsDtos/StudentForCreation.cs
@@ -1,4 +1,20 @@
-﻿namespace Student_Registration.Webui.Dtos.StudentsDtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Student_Registration.Webui.Dtos.StudentsDtos
 {
-    public record StudentForCreation(string Name, int? Age, string? Address, string? Gender, string? PhoneNum);
+    public record StudentForCreation
+    {
+
+        // I use the fluent validation library to validate the data
+        //[Required(ErrorMessage ="You should provide a name value.")]
+        //[MaxLength(50)]
+        public string Name { get; set; } = string.Empty; //  public string Name { get; init; }
+        
+        //[Range(10, 30, ErrorMessage = "Age must be between 10 and 30.")]
+        public int? Age { get; set; }
+        public string? Address { get; set; }
+        public string? Gender { get; set; }
+        public string? PhoneNum { get; set; }
+
+    }
 }
---
+++ Student_Registration.Webui/Dtos/StudentsDtosValidator/StudentForCreationValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Student_Registration.Webui.Dtos.StudentsDtos;
+
+namespace Student_Registration.Webui.Dtos.StudentsDtosValidator
+{
+    public class StudentForCreationValidator : AbstractValidator<StudentForCreation>
+    {
+        public StudentForCreationValidator()
+        {
+            RuleFor(student => student.Name).NotEmpty().WithMessage("You should provide a name value.").MaximumLength(50);
+
+            RuleFor(student => student.Age).InclusiveBetween(10, 30).WithMessage("Age must be between 10 and 30.");
+        }
+    }
+}
